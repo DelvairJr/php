1431a0ba9cde79fefc6591f1d47159d42b81a8e2
Delvair jr <delvair.ifms@gmail.com>
2017-12-18 19:30:15 -0300

    Add crud_php

---
+++ crud_php/Layouts/_include/_rodape.php
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+<footer class="page-footer indigo darken-3">
+      <div class="footer-copyright">
+        <div class="container">
+        <p>© 2017 Copyright Delvair</p>
+        <p>Github: https://github.com/DelvairJr</p>
+        <p>Email: delvair.ifms@gmail.com</p>
+        </div>
+      </div>
+</footer>
+
+</body>
+</html>
---
+++ crud_php/Paginas/home.php
@@ -0,0 +1,66 @@
+<main class="container">
+	
+<form action="busca" method="GET"  >
+	<input type="text" name="busca" placeholder="Busca">
+	<button class="btn indigo">Pesquisar</button>
+</form>
+
+<h2>Lista de Produtos</h2>
+
+<table class="bordered">
+    <thead>
+      <tr>
+          <th>Título</th>
+          <th>Descrição</th>
+          <th>R$</th>
+          <th>Ação</th>
+      </tr>
+    </thead>
+
+    <tbody>
+    	<?php foreach($produtos as $produto): ?>
+      <tr>
+
+        <td><?php echo  $produto['titulo'] ?></td>
+        <td><?php echo  $produto['descricao'] ?></td>
+        <td><?php echo  "R$".number_format($produto['valor'],2,",",".") ?></td>
+        <td>
+        	<a class="btn orange" href="/produto/editar?id=<?php echo $produto['idprodutos'] ?>">Editar</a>        	
+        	<a class="btn red" href="/produto/deletar?id=<?php echo $produto['idprodutos'] ?>">Deletar</a>        	
+        </td>
+        
+      </tr>
+      <?php endforeach; ?>
+      
+    </tbody>
+ </table>
+
+<?php if(isset($editando)): ?>
+	<h2>Editando Produto</h2>
+<?php else: ?>
+	<h2>Adicionar Produto</h2>
+<?php endif; ?>
+
+<?php if(isset($msg)): ?>
+<p><?php echo $msg ?></p>
+<?php endif ?>
+
+<form action="<?php echo (isset($editando) ? '/produto/salvar' : '/produto/adicionar') ?>" method="post">
+
+	<?php if(isset($editando)): ?>
+		<input type="hidden" name="id" value="<?php echo $produtoEdit['idprodutos'] ?>">
+	<?php endif; ?>
+
+	<input type="text" name="titulo" placeholder="Título" 
+	value="<?php echo (isset($produtoEdit['titulo'])? $produtoEdit['titulo'] : ''); ?>">
+	<input type="text" name="descricao" placeholder="Desscrição"
+	value="<?php echo (isset($produtoEdit['descricao'])? $produtoEdit['descricao'] : ''); ?>">
+	<input type="text" name="valor" placeholder="valor R$"
+	value="<?php echo (isset($produtoEdit['valor'])? $produtoEdit['valor'] : ''); ?>">
+	<button class="btn indigo"><?php echo (isset($editando) ? 'Atualizar' : 'Adicionar') ?></button>
+	<?php if(isset($editando)): ?>
+		<a class="btn orange" href="/home">Cancelar</a>
+	<?php endif; ?>
+</form>
+
+</main>
